b5aa364563c233f44692b387ced0389b13613316
Mileto Di Marco <mileto.di.marco@howest.be>
2025-04-01 08:43:37 +0200

    Add project files.

---
+++ cu.staticBasics.cons/Extensions/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu.staticBasics.cons.Extensions
+{
+    public static class IntExtensions
+    {
+        public static int Add(this int number,int numberToAdd)
+        {
+            return number + numberToAdd;
+        }
+        public static int CustomAverage(this IEnumerable<int> values)
+        {
+            int total = 0;
+            foreach(var value in values)
+            {
+                total += value;
+            }
+            return total / values.Count();
+        }
+    }
+}
---
+++ cu.staticBasics.cons/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cu.staticBasics.cons.Extensions;
+
+Console.WriteLine("ExtensionMethods!");
+int number = 2;
+var result = number.Add(2);
+Console.WriteLine(result);
+var numbers = new List<int> {1,2,3,4,56,89,78,56,2 };
+var average = numbers.CustomAverage();
+Console.WriteLine(average);
+
+
+
